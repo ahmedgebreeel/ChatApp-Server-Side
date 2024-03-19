f7aaa558cdc22db326223b1d35901f38cfbdd7bb
ibthal409 <96615454+ibthal92abdulghany@users.noreply.github.com>
2024-03-20 00:37:04 +0200

    get messages in groupChat

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -15,6 +15,12 @@ const sendMessage = asyncHandler(async (req, res, next) => {
   });
   await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: newMessage });
   res.status(201).json({ newMessage })
+});
+const getMessages = asyncHandler(async (req, res, next) => {
+  const { chatId } = req.params
+  const message = await Message.find({ chat: chatId })
+  res.status(200).json(message)
+
 })
 
-module.exports = { sendMessage };
+module.exports = { sendMessage, getMessages };
